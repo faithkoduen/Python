3d81fe394234315bc715eb9be588565f220a2bca
Faith Koduen <faithkoduen@gmail.com>
2025-04-20 19:41:46 +0300

    added file

---
+++ Day1/helloworld.py
@@ -0,0 +1,20 @@
+#Introduction
+print(4+7)
+print(6-4)
+print(5*3)
+print(9/3)
+print(4**3)
+print(8%2)
+print(4//2)
+
+#Checking data type
+print(type(2))
+print(type('Faith'))
+print(type(4.24))
+print(type(4.1234567890123456))
+print(type([5,9,5]))
+print(type(['Python','Kotlin','Javascript']))
+print(type({'name':'Faith','age':22}))
+print(type({1,3,6}))
+print(type(3,78,34,12))
+
